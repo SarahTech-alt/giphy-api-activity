bc073334f0d5cf31750002941b22880a25016ed4
Sarah Fuoss <sfuoss@gmail.com>
2021-09-28 16:37:23 -0500

    Created GET route in the server and App.js

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 const randomRouter = require('./routes/random.router');
+const { default: axios } = require('axios');
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.urlencoded({extended: true}));
@@ -16,4 +17,13 @@ app.use('/random', randomRouter);
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
     console.log('Listening on port: ', port);
+});
+
+app.get('/giphy', (req,res) => {
+axios.get('/gihttps://api.giphy.com/v1/gifs/random?api_key=ErC6fczObj4srp6cMPEQjlMMEB6k8hLA&tag=&rating=gphy')
+    .then(response => {
+        res.send(response.data);
+    }).catch(error => {
+        res.sendStatus(500);
+    })
 });
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,32 @@
+import axios from 'axios';
 import React from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  // Renders the entire app on the DOM
+  const [results, setResults] = useState([]);
+
+  const fetchGiphyResults = () => {
+    axios({
+      method: 'GET',
+      URL: '/giphy'
+    }).then(response => {
+      console.log(response);
+      setResults(response.data.data);
+    })
+  }
+
+
+  useEffect(() => {
+    fetchGiphyResults();
+  }, []);
+
   return (
     <div>
       <header className="App-header">
         <h1>Random Giphy API</h1>
       </header>
-      
+      {JSON.stringify(results)}
       <p>Results go here</p>
     </div>
   );
